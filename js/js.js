cf03b8d767549122afaf8fa78e670a31032c2bcc
TupeDD <tuoppi661@gmail.com>
2016-02-04 18:38:10 +0200

    Sec

--- js/js.js
+++ js/js.js
@@ -16,11 +16,31 @@ var rand3 = fruits3[Math.floor(Math.random() * fruits3.length)];
 
 
 function pelaa() {
-  document.getElementById("fruit1").src = rand1;
-  document.getElementById("fruit2").src = rand2;
-  document.getElementById("fruit3").src = rand3;
-  peli();
   update();
+  if (raha >= 10) {
+    if (panos >= 10) {
+      if (raha >= panos) {
+        document.getElementById("fruit1").src = rand1;
+        document.getElementById("fruit2").src = rand2;
+        document.getElementById("fruit3").src = rand3;
+        peli();
+        update();
+      }
+      else {
+        alert("You dont have enough money.");
+        update();
+      }
+    }
+    else {
+      alert("Choose stake first 10, 50 or 100.");
+      update();
+    }
+  }
+  else {
+    alert("Game Over");
+    update();
+  }
+
 }
 
 function update(){
@@ -28,11 +48,13 @@ function update(){
 }
 
 function vaihdaPanos(uusiPanos){
+  update();
   panos = uusiPanos;
   if (panos == 10) {
   document.getElementById('button1').style.backgroundColor='#333333';
   document.getElementById('button2').style.backgroundColor='#808080';
   document.getElementById('button3').style.backgroundColor='#808080';
+
   }
   else if (panos == 50) {
   document.getElementById('button2').style.backgroundColor='#333333';
@@ -47,17 +69,26 @@ function vaihdaPanos(uusiPanos){
 }
 
 function peli() {
+  update();
   if(rand1 == rand2 && rand2 == rand3) {
     raha += (panos * 10);
-    alert("You won the jack!!!!OMGOMGOMGTROLOLOOO");
+    update();
+    alert("You won the JACKPOT !!!!!!!");
   }
   else if (rand1 == rand2 || rand2 == rand3 || rand3 == rand1) {
     raha += (panos * 5);
-    alert("You won semi jackpot, niche!");
+    update();
+    alert("You won semi jackpot, nice!");
   }
   else {
-    raha - panos;
+    raha -= panos;
+    update();
     alert("You didn't win, sry...");
+    if (raha < 10) {
+      alert("Game Over");
+      if (confirm("Do you want to play again?") == true) {
+        location.reload();
+      }
+    }
   }
 }
-update();
